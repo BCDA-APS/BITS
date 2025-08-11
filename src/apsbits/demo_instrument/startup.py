660aa7f979cdafc592c18a61fd792730ce90b41e
Eric Codrea <codrea.eric@gmail.com>
2025-08-11 14:42:54 -0500

    made tiled client for demo

--- src/apsbits/demo_instrument/startup.py
+++ src/apsbits/demo_instrument/startup.py
@@ -13,6 +13,8 @@ Includes:
 import logging
 from pathlib import Path
 
+from tiled.client import from_uri
+
 from apsbits.core.best_effort_init import init_bec_peaks
 from apsbits.core.catalog_init import init_catalog
 from apsbits.core.instrument_init import make_devices
@@ -63,7 +65,11 @@ register_bluesky_magics()
 # oregistry.clear()
 bec, peaks = init_bec_peaks(iconfig)
 cat = init_catalog(iconfig)
-RE, sd = init_RE(iconfig, bec_instance=bec, cat_instance=cat)
+with open("/home/xf31id/.ipython/profile_blop_flyscan/startup/api_key.txt", 'r') as file:
+    key = file.readline().strip()
+tiled_client = from_uri("http://localhost:8842", api_key=key)
+
+RE, sd = init_RE(iconfig, bec_instance=bec, cat_instance=cat, tiled_client_instance=tiled_client)
 
 
 # Optional Nexus callback block
