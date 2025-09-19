25049c5160db180594f022e916ad153fb71e8ca9
mdecarabas <codrea.eric@gmail.com>
2025-09-19 13:05:49 -0500

    test

--- src/apsbits/demo_instrument/startup.py
+++ src/apsbits/demo_instrument/startup.py
@@ -127,3 +127,4 @@ setup_baseline_stream(sd, oregistry, connect=False)
 from .plans.sim_plans import sim_count_plan  # noqa: E402, F401
 from .plans.sim_plans import sim_print_plan  # noqa: E402, F401
 from .plans.sim_plans import sim_rel_scan_plan  # noqa: E402, F401
+from .plans.sim_plans import sim_scan_plan  # noqa: E402, F401
